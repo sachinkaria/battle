ced957b181385a40818db03193dc16f317c325c5
Chris Parker <cjp214@gmail.com>
2016-03-15 16:07:35 +0000

    added web_helper to dry out tests

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'Checking hit points' do
   scenario 'Returns player 2s hit points' do
-    visit ('/')
-    fill_in :player_1, with: 'Chris'
-    fill_in :player_2, with: 'Sachin'
-    click_button('Start Battle')
+    sign_in_and_play
     expect(page).to have_content('Sachin has 60/60 HP')
   end
 end
