a6559ad42499da31aa4c2ea8729eb31b6c1a7c46
Sachin Karia <sachin.karia01@gmail.com>
2016-03-16 11:47:46 +0000

    created session and redirected to /play

--- Battle/app.rb
+++ Battle/app.rb
@@ -3,13 +3,20 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+enable :sessions
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @warrior_1 = params[:warrior_1]
-    @warrior_2 = params[:warrior_2]
+    session[:warrior_1] = params[:warrior_1]
+    session[:warrior_2] = params[:warrior_2]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @warrior_1 = session[:warrior_1]
+    @warrior_2 = session[:warrior_2]
     erb(:play)
   end
 
